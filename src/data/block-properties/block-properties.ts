59cf11004df4bc2c4e7f045ca7c4aecd8056e6fe
August Saint Freytag <me@augustfreytag.com>
2024-10-23 15:05:12 +0200

    Introduce type definitions for Optifine properties

---
+++ src/data/block-properties/block-properties.ts
@@ -0,0 +1,59 @@
+import { integer } from "@deepkit/type"
+
+export type BlockId = string
+
+export type BiomeId = string
+
+export type BlockTextureTiles = string
+
+export type BlockTextureMatchTiles = string
+
+export type BlockTextureTileWeights = string
+
+export type BlockTintIndex = integer
+
+export type BlockTextureLayer = "cutout_mipped" | "cutout" | "translucent"
+
+export type BlockTextureSymmetry = "none" | "opposite" | "all"
+
+export type BlockTextureMethod =
+	| "ctm"
+	| "ctm_compact"
+	| "horizontal"
+	| "vertical"
+	| "horizontal+vertical"
+	| "vertical+horizontal"
+	| "top"
+	| "random"
+	| "repeat"
+	| "fixed"
+	| "overlay_ctm"
+	| "overlay_random"
+	| "overlay_repeat"
+	| "overlay_fixed"
+
+export type BlockFace = "bottom" | "top" | "north" | "south" | "east" | "west" | "sides" | "all"
+
+export type BlockTextureConnect = "block" | "tile" | "state"
+
+// Properties
+
+export class ConnectedTextureProperties {
+	constructor(
+		public method: BlockTextureMethod, // Required method for connecting textures
+		public tiles: BlockTextureTiles, // List of tiles (as string paths)
+		public matchBlocks?: Set<BlockId>, // Set of block names (e.g., "dirt", "minecraft:oak_stairs")
+		public matchTiles?: BlockTextureMatchTiles, // List of tile names this method should apply to
+		public connect: BlockTextureConnect = "block", // Default: block for blocks, tile for tiles
+		public faces?: BlockFace[], // Optional faces to apply connection
+		public biomes?: Set<BiomeId>, // Optional list of biomes
+		public heights?: Set<integer>, // Optional height restrictions
+		public tintIndex: BlockTintIndex = -1, // Default: -1 (disabled)
+		public tintBlock?: BlockId, // Optional block used for tinting
+		public layer: BlockTextureLayer = "cutout_mipped", // Default render layer is cutout_mipped
+		public weights?: BlockTextureTileWeights, // Optional weights for random textures
+		public randomLoops?: integer, // Optional random loops for random textures
+		public symmetry?: BlockTextureSymmetry, // Optional symmetry option
+		public linked: boolean = false // Default: false
+	) {}
+}
