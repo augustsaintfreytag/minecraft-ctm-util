557832b6bc614b7fde085c8c0154d5edf1e8cdcc
August Saint Freytag <me@augustfreytag.com>
2024-10-25 11:53:46 +0200

    Establish "rewrite" command in app entry point

--- app.ts
+++ app.ts
@@ -1,12 +1,18 @@
+import "~/types/extensions"
+
 import { App } from "@deepkit/app"
 import { FrameworkModule } from "@deepkit/framework"
 import { JSONTransport, Logger } from "@deepkit/logger"
-
-import "~/types/extensions"
-
 import { AppConfig } from "~/app/config"
-import { readFiles } from "~/data/io/file-list"
-import { DirectoryPath } from "~/data/paths/paths"
+import { readResourcePackFiles } from "~/data/io/functions/manifest-file-list"
+import { DirectoryPath } from "~/data/io/library/paths"
+import { manifestPresetByBlockId } from "~/data/manifest/functions/manifest-presets"
+import { readAllManifests } from "~/data/manifest/functions/manifest-read"
+import { decodeManifestFromData } from "~/data/io/functions/manifest-file-coding"
+import { BlockTextureManifestRecord } from "~/data/manifest/models/manifest-record"
+import { writeAllManifests } from "~/data/manifest/functions/manifest-write"
+import { ManifestFileEntry } from "~/data/io/library/manifest-file-entry"
+import { join as joinPath, normalize as normalizePath, sep as pathSeparator } from "path"
 
 new App({
 	config: AppConfig,
@@ -24,10 +30,32 @@ new App({
 		}
 	})
 	.command("list", async (path: DirectoryPath) => {
-		const fileEntries = await readFiles(path)
+		const fileEntries = await readResourcePackFiles(path)
 
 		fileEntries.forEach(fileEntry => {
-			console.log(`Namespace: '${fileEntry.namespace}', Group: '${fileEntry.group}', File: '${fileEntry.file}', Overlay: ${fileEntry.overlay}`)
+			console.log(`Namespace: '${fileEntry.namespace}', File: '${fileEntry.file}'`)
 		})
+	})
+	.command("analyze", async (path: DirectoryPath) => {
+		// Command reserved to check correct hierarchy (no circular references).
+	})
+	.command("rewrite", async (path: DirectoryPath) => {
+		console.log(`Reading manifest presets from resource pack root '${path}'.`)
+
+		const manifestPresets = manifestPresetByBlockId()
+		const manifestRecords = await readAllManifests(path)
+
+		const changedManifestRecords = manifestRecords.compactMap(manifestRecord => {
+			const originalEntry = manifestRecord.entry
+			const presetManifest = manifestPresets.get(originalEntry.id)
+
+			if (!presetManifest) {
+				return undefined
+			}
+
+			return new BlockTextureManifestRecord(originalEntry, presetManifest)
+		})
+
+		writeAllManifests(changedManifestRecords)
 	})
 	.run()
